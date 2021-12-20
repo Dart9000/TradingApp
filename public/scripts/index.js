ffcc4f52830fad08dc55153d335de37992135cae
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2021-12-20 20:03:35 +0530

    User System Integrated

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -29,35 +29,35 @@ function addNavBar(){
 nav.addEventListener("click", addNavBar);
 
 
-// const otpBtn = $("#otpBtn");
-// const regBtn = $("#regBtn");
-// let otp;
-//
-// otpBtn.on("click", () => {
-//
-//   const emailId = $("#username").val();
-//   if(emailId != ""){
-//     let url = "https://talk9api.herokuapp.com/OTP?id="+emailId;
-//
-//     $.getJSON(url, (data) => {
-//       otp = data.OTP;
-//     });
-//
-//     const alertMessage = "Check OTP on your email Id: "+emailId;
-//     alert(alertMessage);
-//     otpBtn.css("display", "none");
-//   }else{
-//     alert("Please Fill all the fields");
-//   }
-// });
-//
-// regBtn.on("click", (e) => {
-//   const otpVal = $("#otp").val();
-//
-//   console.log("OTP", otp);
-//   if(otpVal != otp){
-//     e.preventDefault();
-//     alert("OTP is not correct, try again!!");
-//     otpBtn.css("display", "inline-block");
-//   }
-// });
+const otpBtn = $("#otpBtn");
+const regBtn = $("#regBtn");
+let otp;
+
+otpBtn.on("click", () => {
+
+  const emailId = $("#username").val();
+  if(emailId != ""){
+    let url = "https://talk9api.herokuapp.com/OTP?id="+emailId;
+
+    $.getJSON(url, (data) => {
+      otp = data.OTP;
+    });
+
+    const alertMessage = "Check OTP on your email Id: "+emailId;
+    alert(alertMessage);
+    otpBtn.css("display", "none");
+  }else{
+    alert("Please Fill all the fields");
+  }
+});
+
+regBtn.on("click", (e) => {
+  const otpVal = $("#otp").val();
+
+  console.log("OTP", otp);
+  if(otpVal != otp){
+    e.preventDefault();
+    alert("OTP is not correct, try again!!");
+    otpBtn.css("display", "inline-block");
+  }
+});
